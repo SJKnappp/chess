04f495480700768e67f8c92b43ddaa98e2717687
Samuel Knapp <samuel.j.knapp@btinternet.com>
2019-12-25 16:37:46 +0000

    fixed struct data storage

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,93 @@
-//use std::io;
-use std::{io, mem};
 
+extern crate ansi_term;
 
+use ansi_term::Colour;
+use std::{io, mem, vec, write};
+    
+#[derive(Copy, Clone)]
 struct Piece{
-    peice : char
+    peice : char, // 
+    colour : u8, //0 no specify 1 black 2 white
+    moved : bool,
 }
 
-impl Default for Piece {
-    #[inline]
-    fn default() -> Piece {
-        Piece{
-            peice: ' ',
-        }
-    }
-}
-
-mut struct Board{
-    tile :  Piece, //[[Piece; 8];8],
-    count : u32,
+struct Board{
+    tile :  [[Piece; 8]; 8],
 }
 
 impl Board {
-    fn initate(mut self) -> Board{
-        self.tile = Piece {peice: ' '};
-        self.count = 0;
-        return self;
+    fn new() -> Board{
+        let mut temp = [[Piece{peice: ' ', colour : 0, moved : false};8];8];
+        
+        temp[0][0].peice = 'r';
+        temp[1][0].peice = 'n';
+        temp[2][0].peice = 'b';
+        temp[3][0].peice = 'q';
+        temp[4][0].peice = 'k';
+        temp[5][0].peice = 'b';
+        temp[6][0].peice = 'n';
+        temp[7][0].peice = 'r';
+        temp[0][1].peice = 'p';
+        temp[1][1].peice = 'p';
+        temp[2][1].peice = 'p';
+        temp[3][1].peice = 'p';
+        temp[4][1].peice = 'p';
+        temp[5][1].peice = 'p';
+        temp[6][1].peice = 'p';
+        temp[7][1].peice = 'p';
+        temp[0][6].peice = 'p';
+        temp[1][6].peice = 'p';
+        temp[2][6].peice = 'p';
+        temp[3][6].peice = 'p';
+        temp[4][6].peice = 'p';
+        temp[5][6].peice = 'p';
+        temp[6][6].peice = 'p';
+        temp[7][6].peice = 'p';
+        temp[0][7].peice = 'r';
+        temp[1][7].peice = 'n';
+        temp[2][7].peice = 'b';
+        temp[3][7].peice = 'q';
+        temp[4][7].peice = 'k';
+        temp[5][7].peice = 'b';
+        temp[6][7].peice = 'n';
+        temp[7][7].peice = 'r';
+        
+        for j in 0..2{
+            for i in 0..8{
+                temp[i][j].colour = 2;
+                temp[i][7-j].colour = 1;
+            }
+        }
+        
+        
+        let board = Board { tile: temp,};
+        return board;
     }
-}
-
-fn output(board: Board) {
-    for i in 0..8 {
-        for j in 0..8 {
-            //println!("{:?}", board.board[i][j).peice;
-            println!("board print");
+    fn print(&self){
+        println!(" print line");
+        
+        for j in 0..8{
+            for i in 0..8{
+                //print!(" {} |", if self.tile[i][j].colour == 2 { Colour::Red.paint( self.tile[i][j].peice)} else { Colour::White.paint(self.tile[i][j].peice) },);
+                 //   string = 
+                //let strings: &[ANSIString<'static>] = &[
+                 //       if self.tile[i][j].colour == 2 { Colour::Red.paint(self.tile[i][j].peice)} else { Colour::White.paint(self.tile[i][j].peice) }, ];
+                        
+                print!("{}", strings);
+                    
+            }
+            println!("");
+            print!("--------------------------------");
+            println!("")
         }
     }
-}
-
-unsafe fn initials() -> Board{
-
-       // let board: Board = mem::uninitialized();
-    
-    
-    let mut board : Board;
-    board.tile.peice = ' ';
-    //println!("{:?}", board.tile.peice);
-    for i in 0..8 {
-        for j in 0..8 {
-           // board[i].peice = ' ';
-        }
-    }
-    
-    return board;
-    
 }
 
 fn main() {
     
     let running = true;
-    let board: Board;    
-    unsafe{
-    board = initials();
-    }
-   // output(board);
+    let board = Board::new();
+    board.print();
     
     while running {
         //output;
