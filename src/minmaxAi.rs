62e381e15b1a961704db4bc43fecf161d200d4e4
Samuel Knapp <samuel.j.knapp@btineternet.com>
2019-12-29 02:45:51 +0000

    changed file structer and spilt into muilple files

--- src/minmaxAi.rs
+++ src/minmaxAi.rs
@@ -1,34 +1,35 @@
-mod main;
+pub use main;
+pub mod dataStructs;
 
-fn scores(peice : char) -> u8 {
+fn scores(peice : char) -> i16 {
     match peice{
-        'p' => return 10,
-        'r' => return 500,
-        'n' => return 250,
-        'b' => return 300,
-        'q' => return 1000,
-        'k' => return 10000,
+        'p' => return 1,
+        'r' => return 50,
+        'n' => return 25,
+        'b' => return 30,
+        'q' => return 100,
+        'k' => return 255,
+        _ => return 0,
     }
 }
 
-
-fn calcScore(baord : &Board, colour : u8, check : Check, sphere : [[Sphere;8];8]){
+pub fn calcScore(board : dataStructs::Board, colour : u8, check : dataStructs::Check, sphere : [[dataStructs::Sphere;8];8]){
     let mut openent : u8;
     if  colour == 1 {openent = 2;} else {openent = 1;}
     
-    let mut gain = [[i8; 8]; 8];
-    let mut risk = [[i8; 8]; 8];
+    let mut gain : [[i16; 8]; 8] = [[0;8];8];
+    let mut risk : [[i16; 8]; 8] = [[0;8];8];
 
     //risk current
     for j in 0..8{
         for i in 0..8 {
-            if board.tile[j][i].colour == colour{gain += scores(board.tile[i][j].colour);}
-            if board.tile[j][i].colour == openent{risk += scores(board.tile[i][j].colour);}
+            if board.tile[j][i].colour == colour{gain[i][j] += scores(board.tile[i][j].peice);}
+            if board.tile[j][i].colour == openent{risk[i][j] += scores(board.tile[i][j].peice);}
 
-            print!("{}", risk);
+            print!("{}", risk[i][j]);
         }
-        println("");
+        println!("");
     }
 
+    
 }
-
